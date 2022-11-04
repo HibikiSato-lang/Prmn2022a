d5812a7711c15409771885cded392a4c55319d46
[HibikiSato-lang] <[b2211120@photon.chitose.ac.jp]>
2022-11-04 13:59:20 +0900

    課題

---
+++ Prmn2022a/Prmn2022a/src/lecture01/kadai1_2.java
@@ -0,0 +1,10 @@
+package lecture01;
+
+public class kadai1_2 {
+    public static void main(String[] args) {
+
+        int studentNumber = 2211120;
+
+        System.out.println("B" + studentNumber + " Hibiki Sato");
+    }
+}
